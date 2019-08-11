0bd2985092a26057876360c3eccd1825ba815036
WJLIDDY <wj.liddy@gmail.com>
2019-08-10 20:18:53 -0400

    Simple skelegunner AI

---
+++ Assets/Scripts/GunnerEnemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunnerEnemy : Enemy
+{
+    public bool pursuePlayer;
+    public float pursuePlayerRange;
+    bool aggro;
+    public float yTolerance;
+
+    public override void DoAI()
+    {
+        if (aggro)
+        {
+            fightPlayer();
+        }
+        else
+        {
+            if (Vector2.Distance(GameObject.FindGameObjectWithTag("player").transform.position, this.transform.position) < pursuePlayerRange)
+            {
+                aggro = true;
+            }
+            else
+            {
+                patrol();
+            }
+        }
+    }
+
+    public void fightPlayer()
+    {
+        // face player if we arent.
+        bool playerDir = GameObject.FindGameObjectWithTag("player").transform.position.x > this.transform.position.x;
+        if(facedRight != playerDir)
+        {
+            Move(facedRight ? -1 : 1);
+        } else
+        {
+            // we are facing player. Now, see if we are on the same Y plane.
+            float ydiff = GameObject.FindGameObjectWithTag("player").transform.position.y - this.transform.position.y;
+
+            if (Mathf.Abs(ydiff) < yTolerance)
+            {
+                // Can shoot player.
+                FirePrimary();
+            } else
+            {
+                if (ydiff > 0)
+                {
+                    // Jump to get into position. 
+                    Jump();
+                }
+            }
+        }
+        
+    }
+
+
+}
--- Assets/Scripts/RunnerEnemy.cs
+++ Assets/Scripts/RunnerEnemy.cs
@@ -13,27 +13,9 @@ public class RunnerEnemy : Enemy
             
         } else
         {
-            ignorePlayerAI();
+            patrol();
         }
     }
 
-    public void ignorePlayerAI()
-    {
-        bool runDir = facedRight;
-        var rb = GetComponent<Rigidbody2D>();
 
-
-
-
-        RaycastHit2D hitWall = Physics2D.Raycast(new Vector2(rb.transform.position.x + ((runDir) ? 1f : -1f) * (rb.GetComponent<BoxCollider2D>().size.x), rb.transform.position.y), Vector2.down, 0.01f, (1 << 8), -Mathf.Infinity);
-
-        RaycastHit2D hitGround = Physics2D.Raycast(new Vector2(rb.transform.position.x + ((runDir) ? 1f : -1f) * (2*rb.GetComponent<BoxCollider2D>().size.x), rb.transform.position.y + rb.GetComponent<BoxCollider2D>().size.y), Vector2.down, 1f, (1<<8), -Mathf.Infinity);
-
-        if (hitGround.collider == null || hitWall.collider != null)
-        {
-            runDir = !runDir;
-        }
-
-        Move(runDir ? 1 : -1);
-    }
 }
