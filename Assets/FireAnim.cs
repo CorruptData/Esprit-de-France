dc99100e616a659b108197fad593cc1c5bbfaef5
WJLIDDY <wj.liddy@gmail.com>
2019-08-11 11:47:25 -0400

    Skeles have prefire anims

---
+++ Assets/FireAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAnim : MonoBehaviour
+{
+    float shootTime = 0f;
+    float totalTime = 1f;
+    Sprite[] shoot;
+    public Sprite spriteOrig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shoot = Resources.LoadAll<Sprite>("skeleshoot/");
+    }
+
+    private void Update()
+    {
+        shootTime += Time.deltaTime;
+        if (shootTime < totalTime)
+        {
+            GetComponent<SpriteRenderer>().sprite = shoot[(int)((shootTime * shoot.Length) / totalTime)];
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = spriteOrig;
+        }
+
+    }
+
+    public void onShoot()
+    {
+        shootTime = 0f;
+    }
+}
--- Assets/Scripts/GunnerEnemy.cs
+++ Assets/Scripts/GunnerEnemy.cs
@@ -8,12 +8,19 @@ public class GunnerEnemy : Enemy
     bool aggro;
     public float yTolerance;
 
+    public FireAnim f;
+
     public float JUMP_COOLDOWN_MAX = 2f;
     public float jumpCooldown = 0f;
 
 
-    public float TEMP_FIRE_COOLDOWN_MAX = 1f;
+    public float TEMP_FIRE_COOLDOWN_MAX = 4f;
     public float TEMP_FIRE_COOLDOWN = 0;
+
+
+    public float FIREAIMTIMEMAX = 1.5f;
+    public float fireaimtime = -1f;
+
     public override void DoAI()
     {
         aggro = Vector2.Distance(GameObject.FindGameObjectWithTag("player").transform.position, this.transform.position) < pursuePlayerRange;
@@ -30,6 +37,13 @@ public class GunnerEnemy : Enemy
 
     public void fightPlayer()
     {
+        //bullet make
+        if(fireaimtime > 0 && (fireaimtime - Time.deltaTime < 0))
+        {
+            FirePrimary();
+        }
+
+        fireaimtime -= Time.deltaTime;
         jumpCooldown -= Time.deltaTime;
         TEMP_FIRE_COOLDOWN -= Time.deltaTime;
 
@@ -47,9 +61,7 @@ public class GunnerEnemy : Enemy
             {
                 if (TEMP_FIRE_COOLDOWN < 0)
                 {
-                    TEMP_FIRE_COOLDOWN = TEMP_FIRE_COOLDOWN_MAX;
-                    // Can shoot player.
-                    FirePrimary();
+                    dofire();
                 }
             } else
             {
@@ -62,6 +74,15 @@ public class GunnerEnemy : Enemy
             }
         }
         
+    }
+
+    public void dofire()
+    {
+        Debug.Log("dofire");
+        TEMP_FIRE_COOLDOWN = TEMP_FIRE_COOLDOWN_MAX;
+        // Can shoot player.
+        f.onShoot();
+        fireaimtime = FIREAIMTIMEMAX;
     }
 
 
