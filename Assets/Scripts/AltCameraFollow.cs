257d0db7dd8b779c91e21bde33a3c0629f775832
WJLIDDY <wj.liddy@gmail.com>
2019-08-10 21:09:02 -0400

    Add alt camera follower (pixel perf cam instead of stretch)

---
+++ Assets/Scripts/AltCameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class AltCameraFollow: MonoBehaviour
+{
+    private int pixelsPerUnit = 100;
+    private Camera _camera;
+    public Player p;
+
+    protected new Camera camera { get { if (_camera == null) { _camera = GetComponent<Camera>(); } return _camera; } }
+
+    protected virtual void Awake()
+    {
+        Calculate();
+    }
+
+    protected virtual void Update()
+    {
+        Calculate();
+        if (p != null)
+        {
+            camera.transform.localPosition = new Vector3(p.transform.localPosition.x, p.transform.localPosition.y, -10);
+        }
+    }
+
+    protected virtual void LateUpdate()
+    {
+        Calculate();
+    }
+
+    private void Calculate()
+    {
+        int zoom = (int)Mathf.Max(1f, (Screen.height / 240f));
+        camera.orthographicSize = ((Screen.height / 2f) / (float)pixelsPerUnit) / (float)zoom;
+    }
+}
